8382774ea245f48b315a75f54105861337f444bc
xogur <snsk656@naver.com>
2025-03-20 17:04:33 +0900

    bash_template_dag 업로드

---
+++ dags/dags_bash_with_template.py
@@ -0,0 +1,30 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+import random
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_bash_with_template",
+    schedule="10 0 * * *", # 크론 스케줄줄
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    
+) as dag:
+    bash_t1 = BashOperator(
+        task_id = 'bash_t1',
+        bash_command='echo "data_interval_end : {{ data_interval_end }} "'
+    ) 
+
+    bash_t2 = BashOperator(
+        task_id = 'bash_t2',
+        env = {
+            'START_DATE' : '{{data_interval_start | ds}}',
+            'END_DATE' : '{{data_interval_end | ds }}'
+        },
+
+        bash_command='echo $START_DATE && echo $END_DATE'
+    )
+
+    bash_t1 >> bash_t2
