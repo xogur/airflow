519776403bbc6e775ba4a3731acd417de6a0f063
xogur <snsk656@naver.com>
2025-03-20 17:31:37 +0900

    airflow타임 테스트용

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *", # 크론 스케줄줄
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=True,
+    
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs) :
+        from pprint import pprint
+        pprint.pprint(kwargs)
+
+    show_templates()
