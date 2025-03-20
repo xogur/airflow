71bf6168a95ed5e3453d7177587ebbe1149c0170
xogur <snsk656@naver.com>
2025-03-20 18:15:19 +0900

    진자 템플릿 사용

---
+++ dags/dags_python_template.py
@@ -0,0 +1,33 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_template",
+    schedule="30 9 * * *", # 크론 스케줄줄
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    
+) as dag:
+    
+    def python_function1(start_date, end_date, **kwargs) :
+        print(start_date)
+        print(end_date)
+        
+    python_t1 = PythonOperator(
+        task_id='python_t1',
+        python_callable=python_function1,
+        op_kwargs= {'start_date' : '{{data_interval_start | ds}}', 'end_date' : '{{date_interval_end | ds}}'}
+    )
+
+    @task(task_id='python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds :' + kwargs['ds'])
+        print('ts :' + kwargs['ts'])
+        print('date_interval_start :' + str(kwargs['data_interval_start']))
+        print('date_interval_end :' + str(kwargs['data_interval_end']))
+        print('task_instance :' + str(kwargs['ti']))
+       
