5755323fbb0c6d56f28d687467081a88dfad7a53
xogur <snsk656@naver.com>
2025-03-21 18:40:23 +0900

    파이썬 오퍼레이터 매크로 사용

--- dags/dags_with_python_macro.py
+++ dags/dags_with_python_macro.py
@@ -12,7 +12,7 @@ with DAG(
     
     @task(task_id='task_using_macros',
           templates_dict = {'start_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
-          'end_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(day=-1)) | ds }}'                  
+          'end_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'                  
                             })
     def get_datetime_macro(**kwargs) :
         templates_dict = kwargs.get('templates_dict') or {}
@@ -29,7 +29,7 @@ with DAG(
 
         data_interval_end = kwargs['data_interval_end']
         prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1 , day = 1)
-        prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(day=-1)
+        prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
         print(prev_month_day_first.strftime('%Y-%m-%d'))
         print(prev_month_day_last.strftime('%Y-%m-%d'))
 
