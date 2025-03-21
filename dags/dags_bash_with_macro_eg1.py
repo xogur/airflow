d717f9e7e4364e3a9d37639a466fe7ed9db34aff
xogur <snsk656@naver.com>
2025-03-21 17:34:37 +0900

    매크로 테스트 수정

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -15,5 +15,5 @@ with DAG(
         env = {'START_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
                'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macors.dateuril.relativedelta(days=1)) | ds }}'
         },
-        bash_command= 'echo "START_DATE : $START_DATE" && "END_DATE : $END_DATE"'
+        bash_command= 'echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE"'
     )
--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -15,5 +15,5 @@ with DAG(
         env = {'START_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}',
                'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds }}'
         },
-        bash_command= 'echo "START_DATE : $START_DATE" && "END_DATE : $END_DATE"'
+        bash_command= 'echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE"'
     )
