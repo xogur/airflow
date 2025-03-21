7ac5b01dba3f1c9e5b72a90f03d1b73f957e54e9
xogur <snsk656@naver.com>
2025-03-21 17:35:58 +0900

    매크로 테스트 수정

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -13,7 +13,7 @@ with DAG(
     bash_task_1 = BashOperator(
         task_id = 'bash_task_1',
         env = {'START_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
-               'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macors.dateuril.relativedelta(days=1)) | ds }}'
+               'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateuril.relativedelta(days=1)) | ds }}'
         },
         bash_command= 'echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE"'
     )
