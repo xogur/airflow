cce9b6dc4e122a2f73f711bfe2a442b513f5b0c4
xogur <snsk656@naver.com>
2025-03-21 17:37:02 +0900

    매크로 테스트 수정

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_with_macro_eg1",
+    dag_id="dags_bash_with_macro_eg2",
     schedule="10 0 * * 6#2", # 크론 스케줄줄
     start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
     catchup=False,
